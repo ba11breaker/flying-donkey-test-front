1782c94902f10f2bbaa0933bcda31923a14e04ba
ba11breaker <15212219417@163.com>
2021-08-07 23:57:08 +1000

    add uploader component for upload page

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType, HttpRequest } from '@angular/common/http';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements AfterViewInit {
 
-  constructor() { }
+  baseUrl = `${environment.baseUrl}/files/upload`;
 
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  request = (item) => {
+    const formData = new FormData();
+    formData.append('file', item.file as any);
+    formData.append('allowedType', 'application');
+    formData.append('allowedSize', '40000000');
+    const req = new HttpRequest('POST', this.baseUrl, formData, {
+      reportProgress: true,
+      withCredentials: true
+    });
+    return this.http.request(req).subscribe(
+      event => {
+        if (event.type === HttpEventType.UploadProgress) {
+
+        }
+      }
+    );
   }
 
+  ngAfterViewInit(): void {
+    const defaultUploads = document.getElementsByTagName("nz-upload-list");
+    if (defaultUploads.length > 0) {
+      const defaultUpload = defaultUploads[0];
+      defaultUpload['hidden'] = true;
+    }
+  }
+
+  handleChange(event) {
+    console.log(event);
+  }
 }
--- src/app/ng-zorro-antd.module.ts
+++ src/app/ng-zorro-antd.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
 
 
 @NgModule({
   exports: [
     NzLayoutModule,
     NzTypographyModule,
-    NzMenuModule
+    NzMenuModule,
+    NzUploadModule
   ],
   providers: [
   ]
